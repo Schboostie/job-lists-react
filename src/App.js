9bb0f80208ea58f77fd7536d90df078ea95ff95f
Shahid Amin <schboostie@gmail.com>
2023-06-29 11:40:14 +0100

    -job-lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import jobs from "./data";
 
-function App() {
+const JobCard = ({ job }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <h3>{job.title}</h3>
+      <p>{job.location}</p>
+      <p>{job.description}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [location, setLocation] = useState("");
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const filteredJobs = jobs.filter((job) => {
+    return (
+      job.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      job.location.toLowerCase().includes(location.toLowerCase())
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by job title"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+        <input
+          type="text"
+          placeholder="Search by location"
+          value={location}
+          onChange={handleLocationChange}
+        />
+      </div>
+      <div className="container">
+        <div className="job-cards">
+          {filteredJobs.map((job) => (
+            <JobCard key={job.id} job={job} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const jobs = [
+  {
+    id: 1,
+    title: "Software Engineer",
+    location: "San Francisco",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 2,
+    title: "Data Analyst",
+    location: "New York",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 3,
+    title: "UX Designer",
+    location: "Seattle",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+];
+
+export default jobs;
